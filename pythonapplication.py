1c27f576fad0ebb7f36ca0223e8c3c01364af139
Srijesh Khanal <khanal.srijesh@gmail.com>
2023-11-14 10:35:24 -0500

    Python Project Practical

---
+++ pythonapplication.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import requests
+
+app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+@app.route('/weatherapp', methods=["POST", "GET"])
+def get_weatherdata():
+    url="https://api.openweathermap.org/data/2.5/weather"
+
+    param={
+        'q':"Brampton",
+        'units': "metrics",
+        'appid':api_key
+    }
+
+    response = requests.get(url, params=param)
+    data = response.json()
+    city=data['name']
+    return f"data: {data}, city:{city}"
+    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", )
+
+
