73f88b800d9b11bb284f1f2da3c970731721c3b8
ostiarun55@gmail.com <ostiarun55@gmail.com>
2023-11-24 07:24:45 -0500

    Updated the code for newsletter

--- app.py
+++ app.py
@@ -7,23 +7,21 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
-@app.route("/movieinfo", methods=["POST"])
-def get_movie_info():
-    movie_name = request.form.get("movie")
-    imdb_api_key = request.form.get('appid')
+@app.route("/newsletterinfo", methods=["POST"])
+def get_newsletter_info():
+    newsletter_name = request.form.get("newsletter")
+    newsletter_api_key = request.form.get('apikey')
 
-    # IMDb API URL
-    url = f"http://www.omdbapi.com/?t={movie_name}&apikey={imdb_api_key}"
+    url = f"http://example.com/newsletter/api/info?name={newsletter_name}&apikey={newsletter_api_key}"
 
     response = requests.get(url)
     data = response.json()
 
-    # Check if the request was successful
-    if response.status_code == 200 and data['Response'] == 'True':
-        return render_template("movie_info.html", movie_data=data)
+    if response.status_code == 200 and data.get('status') == 'success':
+        return render_template("newsletter_info.html", newsletter_data=data)
     else:
-        error_message = data.get('Error', 'An error occurred.')
+        error_message = data.get('error', 'ERROR!!')
         return render_template("error.html", error_message=error_message)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5500)
